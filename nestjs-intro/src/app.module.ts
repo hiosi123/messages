96186857a9d3036b46551bae85cfb23a7b34641d
hong-sije <hong@sijecorp.com>
2025-12-18 21:50:23 +0900

    update

--- nestjs-intro/src/app.module.ts
+++ nestjs-intro/src/app.module.ts
@@ -5,10 +5,27 @@ import { UsersModule } from './users/users.module';
 import { APP_PIPE } from '@nestjs/core';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, PostsModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [],
+      inject: [],
+      useFactory: () => ({
+        type: 'postgres',
+        entities: [],
+        synchronize: true,
+        port: 5432,
+        username: 'simon',
+        password: 'root',
+        host: 'localhost', 
+        database: 'postgres',
+      })
+    }),
+    UsersModule, 
+    PostsModule, 
+    AuthModule],
   controllers: [AppController],
   providers: [
     AppService,
